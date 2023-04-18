279079e83a7f54132e6e05528894490944bfd2b2
Leonardo santos <leonardosantos01@outlook.com>
2023-04-17 22:06:02 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeepKnowledgePlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
